55a49e98aa2f3ff1d54140c15f133955b1899883
dvd <david@chartbeat.com>
2017-04-25 20:07:07 -0400

    get lcd working for news break

--- pyradio.py
+++ pyradio.py
@@ -32,11 +32,13 @@ station['wfmu'] = "http://stream0.wfmu.org/freeform-128k.mp3"
 station['resonance'] = "http://54.77.136.103:8000/resonance"
 station['wbur'] = "http://wbur-sc.streamguys.com/wbur"
 
+news = {}
+
 ## todo, increment this every time a play happens
 next_station = station[(station.keys()[0])]
 
 def init_npr():
-	global station
+	global news
 	#fetch rss feed
 	d = feedparser.parse('https://www.npr.org/rss/podcast.php?id=500005')
 
@@ -45,11 +47,12 @@ def init_npr():
 
 	#mpg123 won't load https and doesn't handle query params well, so this cleans up the url
 	o = urlparse(raw_url)
-	npr = "http://" + o.netloc + o.path
-	station['npr'] = npr
+	clean_url = "http://" + o.netloc + o.path
 
 	#grab the stream title incase we need it later
 	npr_title = d.entries[0].title
+
+	news['npr'] = [npr_title, clean_url]
 
 #plays stream url provided in arg
 def play(s):
@@ -73,7 +76,7 @@ def play_news():
 	off()
 	
 	#play the news (note the k argument in mpg123 is # of frames to skip. 900 should get us past the preroll ad)
-	stream = subprocess.Popen(["mpg123", "-q", "-k 900", station['npr']], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	stream = subprocess.Popen(["mpg123", "-q", "-k 900", news['npr'][1]], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 	stream_status = "news"
 	stream.wait()
 
@@ -88,6 +91,9 @@ def play_news():
 def news_break():
 	t = threading.Thread(target=play_news)
 	t.start()
+	
+	#display most recent news hour title
+	lcd.message(news['npr'][0])
 
 #turns everything off
 def off():
